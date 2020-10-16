9f6fc10ee324aaaa2a455faf5067f09f3c990fe9
cjannusch <claytonjannusch@gmail.com>
2020-10-15 21:54:13 -0500

    first working version

---
+++ Main.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 15 21:28:18 2020
+
+@author: Clayton
+"""
+import wget
+
+print('Beginning file download with requests')
+
+raw = 'https://cfvod.kaltura.com/hls/p/1660902/sp/166090200/serveFlavor/entryId/1_t8hy4wqs/v/1/ev/5/flavorId/1_joh8ocz7/name/a.mp4/seg-2-v1-a1.ts'
+numberPart = raw.find('seg-')
+count = 1
+afterNumberPart = raw.find('-v1-')
+
+url = raw[0:numberPart + 4] + str(count) + raw[afterNumberPart:len(raw)]
+
+filename = 'C:\\Users\\Clayton\\Downloads\\LectureVideo\\' + str(count) + '.ts'
+
+wget.download(url, filename)
+
+for i in range(10000):
+    if i == 0:
+        continue
+    count = i
+    url = raw[0:numberPart + 4] + str(count) + raw[afterNumberPart:len(raw)]
+    filename = 'C:\\Users\\Clayton\\Downloads\\LectureVideo\\' + str(count) + '.ts'
+    wget.download(url, filename)
+    
