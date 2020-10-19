bb664cbbc5c21b59428d9bc5e39dec9c367c1cba
cjannusch <claytonjannusch@gmail.com>
2020-10-19 13:11:04 -0500

    had to add wait so that the clips would mesh properly

--- Main.py
+++ Main.py
@@ -1,24 +1,18 @@
 # -*- coding: utf-8 -*-
 
 import wget
+import time
 
 print('Beginning file download with requests')
 
-raw = 'https://cfvod.kaltura.com/hls/p/1660902/sp/166090200/serveFlavor/entryId/1_t8hy4wqs/v/1/ev/5/flavorId/1_joh8ocz7/name/a.mp4/seg-2-v1-a1.ts'
+raw = 'https://cfvod.kaltura.com/hls/p/1660902/sp/166090200/serveFlavor/entryId/1_gr6opqrk/v/1/ev/5/flavorId/1_f4r6gday/name/a.mp4/seg-8-v1-a1.ts'
 numberPart = raw.find('seg-')
-count = 1
 afterNumberPart = raw.find('-v1-')
-
-url = raw[0:numberPart + 4] + str(count) + raw[afterNumberPart:len(raw)]
-
-filename = 'C:\\Users\\Clayton\\Downloads\\LectureVideo\\' + str(count) + '.ts'
-
-wget.download(url, filename)
 
 for i in range(10000):
     if i == 0:
         continue
-    count = i
-    url = raw[0:numberPart + 4] + str(count) + raw[afterNumberPart:len(raw)]
-    filename = 'C:\\Users\\Clayton\\Downloads\\LectureVideo\\' + str(count) + '.ts'
+    url = raw[0:numberPart + 4] + str(i) + raw[afterNumberPart:len(raw)]
+    filename = 'C:\\Users\\Clayton\\Downloads\\LectureVideo\\' + str(i) + '.ts'
     wget.download(url, filename)
+    time.sleep(.5)
